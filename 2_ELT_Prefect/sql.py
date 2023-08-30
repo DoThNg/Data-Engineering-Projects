b1b6d93d762ce76c1e8f9ef7061841fa04d7ea66
DoThNg <117047600+DoThNg@users.noreply.github.com>
2023-08-30 16:57:42 +1000

    Update sql.py

--- 2_ELT_Prefect/sql.py
+++ 2_ELT_Prefect/sql.py
@@ -1,5 +1,3 @@
-# List of tables to create
-
 # 1. CREATE GREEN-TAXI TABLE
 extension_uuid_sql = ("""
                     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -36,7 +34,6 @@ greenTaxi_create_table_sql = ("""
                         """)
 
 # 2. INSERT RECORDS
-
 insert_trip_record_sql = ("""
 
                 INSERT INTO greentaxi (vendor_id, lpep_pickup_datetime, lpep_dropoff_datetime,
