043eb6224fa7040038c46e67e43be862e23ccf95
DoThNg <117047600+DoThNg@users.noreply.github.com>
2023-09-10 16:50:23 +1000

    Update sql.py

--- 1_PostgreSQL_ETL/sql.py
+++ 1_PostgreSQL_ETL/sql.py
@@ -1,15 +1,14 @@
 # 1. CREATE GREEN-TAXI TABLE
 extension_uuid_sql = ("""
-                    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-                  """)
+                      CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+                      """)
 
 greenTaxi_drop_table_sql = ("""
-                            DROP TABLE IF EXISTS greentaxi;
-                        
-                        """)
+                            DROP TABLE IF EXISTS greentaxi;  
+                            """)
 
 greenTaxi_create_table_sql = ("""   
-                          CREATE TABLE greentaxi
+                                CREATE TABLE greentaxi
                                 (trip_id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
                                 vendor_id float, 
                                 lpep_pickup_datetime timestamp, 
@@ -31,16 +30,14 @@ greenTaxi_create_table_sql = ("""
                                 payment_type float, 
                                 trip_type float, 
                                 congestion_surcharge float);
-                        """)
+                              """)
 
 # 2. INSERT RECORDS
 insert_trip_record_sql = ("""
-
-                INSERT INTO greentaxi (vendor_id, lpep_pickup_datetime, lpep_dropoff_datetime,
-                                        store_and_fwd_flag, ratecode_id, pu_loc_id, do_loc_id,
-                                        passenger_count, trip_distance, fare_amount, extra, mta_tax,
-                                        tip_amount, tolls_amount, ehail_fee, improvement_surcharge,
-                                        total_amount, payment_type, trip_type, congestion_surcharge)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-                           
-                """)
+                            INSERT INTO greentaxi (vendor_id, lpep_pickup_datetime, lpep_dropoff_datetime,
+                                                  store_and_fwd_flag, ratecode_id, pu_loc_id, do_loc_id,
+                                                  passenger_count, trip_distance, fare_amount, extra, mta_tax,
+                                                  tip_amount, tolls_amount, ehail_fee, improvement_surcharge,
+                                                  total_amount, payment_type, trip_type, congestion_surcharge)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+                          """)
