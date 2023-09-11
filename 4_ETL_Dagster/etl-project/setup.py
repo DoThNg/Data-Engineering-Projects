f5279b9cf2cee9577ced96d0b92fdd6d5bb5959d
DoNguyen <thng0027@uni.sydney.edu.au>
2023-09-11 20:37:24 +1000

    Update README.mds

---
+++ 4_ETL_Dagster/etl-project/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="etl_project",
+    packages=find_packages(exclude=["etl_project_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud",
+        "pandas==2.1.0",
+        "psycopg2==2.9.7",
+        "python-dotenv==1.0.0",
+        "pyarrow==13.0.0"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
